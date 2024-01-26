5ed25dc6c7f9d4724ce7ce651fff7e84763ac1cf
moh7386 <104713409+moh7386@users.noreply.github.com>
2024-01-27 00:10:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.gurl = 'https://instagram.com/itz_shaikho/' // add your username
 global.sudo = process.env.SUDO || '923184070915' 
 global.devs = '923184070915';
 global.website = 'https://github.com/salmanytofficial/XLICON-MD' //wa.me/+91000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/3c341828d86ee7a89c73f.jpg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://d.top4top.io/p_2947qv1mj0.jpg'
 module.exports = {
   sessionName:  process.env.SESSION_ID ||  " ",  //PUT Session Id Here, 
   author:  process.env.PACK_AUTHER ||  '',
