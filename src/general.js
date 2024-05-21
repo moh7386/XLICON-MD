1f4abbab6e633e897ce8e5fe98cd9b6b36a89e38
moh7386 <104713409+moh7386@users.noreply.github.com>
2024-05-22 02:46:06 +0300

    Update general.js

--- src/general.js
+++ src/general.js
@@ -14,56 +14,7 @@ const axios = require('axios')
 const speed = require('performance-now')
 const fetch = require('node-fetch');
 //---------------------------------------------------------------------------
-cmd({
-    pattern: "شات",
-    alias :['gpt'],
-    desc: "ذكاء اصنطاعي",
-    category: "الذكاء الاصطناعي",
-    use: '<Hii,Secktor>',
-    filename: __filename,
-},
-async(Void, citel,text) => {
-    let zx = text.length;
-    if (zx < 8) {
-        let {data} = await axios.get(`http://api.brainshop.ai/get?bid=167991&key=aozpOoNOy3dfLgmB&uid=[${citel.sender.split("@")[0]}]&msg=[${text}]`);
-        return citel.reply(data.cnt);  
-    }
-    if (!text) return citel.reply(`Hey there! ${citel.pushName}. How are you doing these days?`);
-    // const { Configuration, OpenAIApi } = require("openai");
-    // const configuration = new Configuration({
-    //     apiKey: Config.OPENAI_API_KEY || "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",
-    // });
-    // const openai = new OpenAIApi(configuration);
-    // const completion = await openai.createCompletion({
-    //     model: "text-davinci-002",
-    //     prompt: text,
-    //     temperature: 0.5,
-    //     max_tokens: 80,
-    //     top_p: 1.0,
-    //     frequency_penalty: 0.5,
-    //     presence_penalty: 0.0,
-    //     stop: ['"""'],
-    // });
-    // citel.reply(completion.data.choices[0].text);
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${Config.OPENAI_API_KEY}`,
-    },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo", 
-      messages: [{ role: "system", content: "You" }, { role: "user", content: text }],
-    }),
-  });
 
-  const data = await response.json();
-  console.log("GPT REPONCE : ",data); 
-  if (!data.choices || data.choices.length === 0) {citel.reply("*Invalid ChatGPT API Key, Please Put New Key*"); }
-  return await  citel.reply(data.choices[0].message.content)
-	
-}
-)
 cmd({
     pattern: "chat",
     alias :['gpt'],
